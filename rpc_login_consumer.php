0090521380b2063554cb3bc2f1ced88a8d0b40cd
Smit Joshi <saj39@njit.edu>
2022-06-27 22:16:21 +0000

    Added updating user profile

--- rpc_login_consumer.php
+++ rpc_login_consumer.php
@@ -25,14 +25,16 @@ function userLogin($n)
 	$params = array(":user_email" => $n["user_email"]);
 	$r = $stmt->execute($params);
 	$e = $stmt->errorInfo();
+	$result = $stmt->fetch(PDO::FETCH_ASSOC);
 	$response = null;
 	if ($e[0] != "00000") {
-		$response = false;
+		$response = array(
+			"status" => "error",
+			"message" => $e[2]
+		);
 	} else {
-		$result = $stmt->fetch(PDO::FETCH_ASSOC);
 
 		if ($result && isset($result["password"]) && $result["is_active"] == 1) {
-
 			$password_hash = $result["password"];
 			if (password_verify($n["pass"], $password_hash)) {
 				unset($result["password"]);
@@ -43,17 +45,18 @@ function userLogin($n)
 					"email" => $result["email"],
 					"id" => $result["id"],
 					"bday" => $result["bday"]
-
 				);
-				/* set_sess_var("fname", $result["fname"]);
-				set_sess_var("lname", $result["lname"]);
-				set_sess_var("username", $result["username"]);
-				set_sess_var("email", $result["email"]);
-				set_sess_var("id", $result["id"]);
-				header("Location:home.php");  */
 			} else {
-				$response = 1;
+				$response = array(
+					"status" => "error",
+					"message" => "Invalid password"
+				);
 			}
+		} else {
+			$response = array(
+				"status" => "error",
+				"message" => "Incorrect credentials"
+			);
 		}
 	}
 	return $response;
@@ -79,11 +82,4 @@ $callback = function ($req) {
 
 $channel->basic_qos(null, 1, null);
 $channel->basic_consume('login_queue', '', false, false, false, false, $callback);
-
-
-while ($channel->is_open()) {
-	$channel->wait();
-}
-
-$channel->close();
-$connection->close();
+$channel->wait();
