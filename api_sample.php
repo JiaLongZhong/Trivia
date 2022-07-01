c86ad9fa6823b2c4d97aa483fc137ab7ef5c6de1
dominic2486 <70656707+dominic2486@users.noreply.github.com>
2022-07-01 14:09:49 -0400

    using the rpc_producer class to simplify sending the question data over MQ

--- api_sample.php
+++ api_sample.php
@@ -45,8 +45,8 @@ if ($err) {
 	else{
 		$response_array = json_decode($response, true);
 	$questions = $response_array["results"];
-	if(isset($questions) && "response_code"==0){
-		$mq_connection = new AMQPConnection($brokerhost, $brokerport, $brokeruser, $brokerpass, $vhost);
+	if(isset($questions) && $response_array["response_code"]==0){
+		/*$mq_connection = new AMQPConnection($brokerhost, $brokerport, $brokeruser, $brokerpass, $vhost);
 		$mq_channel = $mq_connection->channel();
 		$mq_channel->queue_declare('testQueue', false, false, false, false);
 		$mq_channel->exchange_declare('testExchange', 'direct', false, false, false);
@@ -62,7 +62,30 @@ if ($err) {
 			$mq_channel->basic_publish(new AMQPMessage(json_encode($question_array)), 'testExchange');
 		}
 		$mq_channel->close();
-		$mq_connection->close();
+		$mq_connection->close();*/
+
+		require_once(__DIR__ . "/rpc_producer.php");
+            $reg_rpc = new RpcClient();
+			foreach($questions as $question){
+				$question_array = array(
+					"question" => $question["question"],
+					"correct_answer" => $question["correct_answer"],
+					"incorrect_answers" => $question["incorrect_answers"],
+					"category" => $question["category"],
+					"difficulty" => $question["difficulty"]
+				);
+			}
+			$response = json_decode($reg_rpc->call($question_array, "testQueue"),true);
+			if ($response["status"] == "success") {
+                echo "Questions written Successful";
+                //header("Location: login.php");
+            } elseif (
+                $response["status"] == "error"
+            ) {
+                echo "Error.";
+            } else {
+                echo "An error occurred during question encoding. Please try again";
+            }
 	}
 	}
 }
