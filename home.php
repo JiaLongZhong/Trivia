ac2fe2833ad34e6d32afe2e66105b6a81ee5e301
Kero <55101058+JiaLongZhong@users.noreply.github.com>
2022-07-03 18:52:40 -0400

    Update home.php
    update home.php

--- home.php
+++ home.php
@@ -16,10 +16,28 @@ if (!is_logged_in()) {
     <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 
 
-    <?php
-    require_once(__DIR__ . "/lib/helpers.php");
-    echo '<p>Welcome ' . get_user_fullname() . '</p>';
-    ?>
+   <div class="container1">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div id="intro" class="bg-image shadow-2-strong">
+                        <div class="mask" style="background-color: rgba(0, 0, 0, 0.8);">
+                            <div class="container d-flex align-items-center justify-content-center text-center h-100">
+                                <div class="text-white">
+                                    <h1 class="mb-3">IT490 Trivia Project</h1>
+                                    <?php
+                                    require_once(__DIR__ . "/lib/helpers.php");
+                                    echo '<p>Welcome ' . get_user_fullname() . '</p>';
+                                    ?>
+                                    <a class="btn btn-outline-light btn-lg m-2" href="#" role="button" rel="nofollow" target="_blank">Create Room</a>
+                                    <a class="btn btn-outline-light btn-lg m-2" href="#" role="button" rel="nofollow" target="_blank">Join</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
     <form action="logout.php">
         <input type="submit" value="Log Out" />
     </form>
