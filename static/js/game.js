c121d7703732837e15e88898b3ee4c2c65003156
Kero <55101058+JiaLongZhong@users.noreply.github.com>
2022-07-22 17:55:14 -0400

    Update game.js
    add 60 sec timer per question and point system.

--- static/js/game.js
+++ static/js/game.js
@@ -1,22 +1,38 @@
-
 const _question = document.getElementById('question');
 const _options = document.querySelector('.quiz-options');
 const _checkBtn = document.getElementById('check-answer');
 const _playAgainBtn = document.getElementById('play-again');
 const _result = document.getElementById('result');
-const _correctScore = document.getElementById('correct-score');
+const _questionNum = document.getElementById('question-number');
 const _totalQuestion = document.getElementById('total-question');
+const _timer = document.getElementById('timer');
+const _totalScore = document.getElementById('total-score');
 
-let correctAnswer = "", correctScore = askedCount = 0, totalQuestion = 10;
+let correctAnswer = "", questionNum = 0, totalQuestion = 10, point=1000, totalScore=0, mutiplers=1, time = 60, rapidpoint=0;
 
 // load question from API
 async function loadQuestion(){
-    const APIUrl = 'https://opentdb.com/api.php?amount=1';
+    const APIUrl = 'https://opentdb.com/api.php?amount=1&type=multiple';
     const result = await fetch(`${APIUrl}`)
     const data = await result.json();
     _result.innerHTML = "";
     showQuestion(data.results[0]);
 }
+
+//timer sections
+var interval = setInterval(function(){
+  document.getElementById('timer').innerHTML=time;
+  time--;
+  if (time === 0){
+    document.getElementById('timer').innerHTML='Done';
+    mutiplers = 1;
+    //quizconsolelog();
+    _result.innerHTML = `<p><i class = "fas fa-times"></i>Time is up!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
+    checkCount();
+    _checkBtn.disabled = false;
+
+  }
+}, 1500);
 
 // event listeners
 function eventListeners(){
@@ -28,7 +44,9 @@ document.addEventListener('DOMContentLoaded', function(){
     loadQuestion();
     eventListeners();
     _totalQuestion.textContent = totalQuestion;
-    _correctScore.textContent = correctScore;
+    _questionNum.textContent = questionNum;
+    _totalScore.textContent = Math.round(totalScore);
+    _timer.textContent = time;
 });
 
 
@@ -52,6 +70,12 @@ function showQuestion(data){
 
 }
 
+function quizconsolelog(){
+    console.log(`Question:#${questionNum}`); 
+    console.log(`Next Question Mutiplers:${mutiplers}`)
+    console.log(`TotalScore:${totalScore}`);
+}
+
 
 // options selection
 function selectOption(){
@@ -72,9 +96,14 @@ function checkAnswer(){
     if(_options.querySelector('.selected')){
         let selectedAnswer = _options.querySelector('.selected span').textContent;
         if(selectedAnswer == HTMLDecode(correctAnswer)){
-            correctScore++;
+            rapidpoint = 1/time * point;
+            totalScore += (point-rapidpoint) * mutiplers;
+            mutiplers += 0.1;
+            //quizconsolelog();
             _result.innerHTML = `<p><i class = "fas fa-check"></i>Correct Answer!</p>`;
         } else {
+            mutiplers = 1;
+            //quizconsolelog();
             _result.innerHTML = `<p><i class = "fas fa-times"></i>Incorrect Answer!</p> <small><b>Correct Answer: </b>${correctAnswer}</small>`;
         }
         checkCount();
@@ -92,32 +121,38 @@ function HTMLDecode(textString) {
 
 
 function checkCount(){
-    askedCount++;
+   questionNum++;
+    time = 60;
     setCount();
-    if(askedCount == totalQuestion){
+    
+    if(questionNum == totalQuestion){
         setTimeout(function(){
             console.log("");
         }, 5000);
 
-
-        _result.innerHTML += `<p>Your score is ${correctScore}.</p>`;
+        totalScore = Math.round(totalScore);
+        _result.innerHTML += `<p>Your score is ${totalScore}!</p>`;
         _playAgainBtn.style.display = "block";
         _checkBtn.style.display = "none";
+        clearInterval(interval);
     } else {
         setTimeout(function(){
             loadQuestion();
-        }, 300);
+        }, 1000);
     }
 }
 
 function setCount(){
     _totalQuestion.textContent = totalQuestion;
-    _correctScore.textContent = correctScore;
+    _questionNum.textContent = questionNum;
+    _totalScore.textContent = Math.round(totalScore);
+    _timer.textContent = time;
 }
 
 
 function restartQuiz(){
-    correctScore = askedCount = 0;
+    questionNum = 0;
+    totalScore = 0;
     _playAgainBtn.style.display = "none";
     _checkBtn.style.display = "block";
     _checkBtn.disabled = false;
