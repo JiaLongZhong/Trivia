233d85ec4eb0fbbed25bdfce0f7ed8d590d77f81
Kero <55101058+JiaLongZhong@users.noreply.github.com>
2022-07-22 17:59:34 -0400

    Update game.php
    add point system and working timer.

--- game.php
+++ game.php
@@ -7,66 +7,73 @@ if (!is_logged_in()) {
 }
 ?>
 
-    <head>
-        <meta charset="UTF-8">
-        <title>Game Page</title>
-        <?php require_once(__DIR__ . "/partials/gameheader.php"); ?>
-    </head>
-    <body>
-        <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-        <div class = "flex">
-        <div class = "wrapper">
-            <div class = "quiz-container">
-                <div class = "quiz-head">
-                    <h1 class = "quiz-title">Quiz Game</h1>
-                    <div class = "quiz-score flex">
-                        <span id = "correct-score"></span>/<span id = "total-question"></span>
+<head>
+    <meta charset="UTF-8">
+    <title>Game Page</title>
+    <?php require_once(__DIR__ . "/partials/gameheader.php"); ?>
+</head>
+
+<body>
+    <?php require_once(__DIR__ . "/partials/nav.php"); ?>
+    <div class="flex">
+        <div class="wrapper">
+            <div class="quiz-container">
+                <div class="quiz-head">
+                    <h1 class="quiz-title">Quiz Game</h1>
+                    <div class="quiz-number flex">
+                        <span id="question-number">Question:</span>/<span id="total-question"></span>
+                    </div>
+                    <div class="quiz-score flex">
+                        <span>Score:&nbsp </span><span id="total-score">Score:</span>
+                    </div>
+                    <div class="quiz-timer flex">
+                        <span>Timer:&nbsp </span><span id="timer"></span>
                     </div>
                 </div>
-                <div class = "quiz-body">
-                    <h2 class = "quiz-question" id = "question">
+                <div class="quiz-body">
+                    <h2 class="quiz-question" id="question">
                         <!--What is the full form of HTTP? -->
                     </h2>
-                    
-                    <ul class = "quiz-options">
+
+                    <ul class="quiz-options">
                         <!--<li>1. Hyper text transfer package</li>
                         <li>2. Hyper text transfer protocol</li>
                         <li>3. Hyphenation text test program</li>
                         <li>4. None of the above</li>-->
                     </ul>
-                    <div id = "result">
+                    <div id="result">
                     </div>
                 </div>
-                <div class = "quiz-foot">
-                    <button type = "button" id = "check-answer">Check Answer</button>
-                    <button type = "button" id = "play-again">Play Again!</button>
+                <div class="quiz-foot">
+                    <button type="button" id="check-answer">Check Answer</button>
+                    <button type="button" id="play-again">Play Again!</button>
                 </div>
             </div>
 
         </div>
-        </div>
+    </div>
 
-        <div class="button-container">
-            <p class="button-prefix">C</p>
-            <p class="button-text" id="answer3"></p>
-        </div>
-        <div class="button-container">
-            <p class="button-prefix">D</p>
-            <p class="button-text" id="answer4"></p>
-        </div>
+    <div class="button-container">
+        <p class="button-prefix">C</p>
+        <p class="button-text" id="answer3"></p>
+    </div>
+    <div class="button-container">
+        <p class="button-prefix">D</p>
+        <p class="button-text" id="answer4"></p>
+    </div>
 
     </div>
-</div>
-<script>
-    document.getElementById('answer1').innerHTML = <?php echo json_encode($_GET['ia1']); ?>;
-    document.getElementById('answer2').innerHTML = <?php echo json_encode($_GET['ia2']); ?>;
-    document.getElementById('answer3').innerHTML = <?php echo json_encode($_GET['ia3']); ?>;
-    document.getElementById('answer4').innerHTML = <?php echo json_encode($_GET['ia4']); ?>;
-    document.getElementById('question').innerHTML = <?php echo json_encode($_GET['q']); ?>;
-    document.getElementById('category').innerHTML = <?php echo json_encode($_GET['c']); ?>;
-    document.getElementById('difficulty').innerHTML = <?php echo json_encode($_GET['d']); ?>;
+    </div>
+    <script>
+        document.getElementById('answer1').innerHTML = <? php echo json_encode($_GET['ia1']); ?>;
+        document.getElementById('answer2').innerHTML = <? php echo json_encode($_GET['ia2']); ?>;
+        document.getElementById('answer3').innerHTML = <? php echo json_encode($_GET['ia3']); ?>;
+        document.getElementById('answer4').innerHTML = <? php echo json_encode($_GET['ia4']); ?>;
+        document.getElementById('question').innerHTML = <? php echo json_encode($_GET['q']); ?>;
+        document.getElementById('category').innerHTML = <? php echo json_encode($_GET['c']); ?>;
+        document.getElementById('difficulty').innerHTML = <? php echo json_encode($_GET['d']); ?>;
     </script>
-<?php
+    <?php
 
 if (isset($_GET["c"])) {
     $question = $_GET["q"];
@@ -114,8 +121,8 @@ if ($response["status"] == "error") {
 }
 }
 ?>
-<?php include_once(__DIR__ . "/partials/footer.php"); ?>
-<script src="static/js/game.js"></script>
+    <?php include_once(__DIR__ . "/partials/footer.php"); ?>
+    <script src="static/js/game.js"></script>
 </body>
-</html>
 
+</html>
