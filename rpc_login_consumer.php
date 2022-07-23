5c7199a1349268cdc57402c6543ea7296ddb724f
Smit Joshi <saj39@njit.edu>
2022-07-23 20:16:37 +0000

    Changingfile extension forr consumers

--- rpc_login_consumer.php
+++ rpc_login_consumer.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once(__DIR__ . '/lib/configrmq.ini');
+require_once(__DIR__ . '/lib/configrmq.php');
 require_once(__DIR__ . "/lib/helpers.php");
 require_once __DIR__ . '/vendor/autoload.php';
 
