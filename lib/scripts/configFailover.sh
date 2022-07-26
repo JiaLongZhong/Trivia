3351d6cb7648d85d8ca5f86d5e6ed349186c50c0
dominic2486 <70656707+dominic2486@users.noreply.github.com>
2022-07-26 12:52:07 -0400

    final

--- lib/scripts/configFailover.sh
+++ lib/scripts/configFailover.sh
@@ -5,4 +5,4 @@ sed -i "s/$1/$2/g" /etc/rsyslog.conf | sudo tee /etc/rsyslog.conf
 service rsyslog restart
 sed -i "s/$1/$2/g" /home/$USER/live/lib/configrmq.ini >> /home/$USER/live/lib/configrmq.ini
 #sed -i "s/$1/$2/g" /etc/hosts | tee /etc/hosts
-[ -e /home/$USER/live/lib/config.ini ] && { sed -i "s/$1/$2/g" /home/$USER/live/lib/config.ini; } 
+#[ -e /home/$USER/live/lib/config.ini ] && { sed -i "s/$1/$2/g" /home/$USER/live/lib/config.ini; } 
