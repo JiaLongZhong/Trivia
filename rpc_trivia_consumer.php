da4fd3da0557eed93cf917ac2ec26b96beb04a76
Smit Joshi <saj39@njit.edu>
2022-07-27 21:57:47 +0000

    Trivia name and description can be edited

--- rpc_trivia_consumer.php
+++ rpc_trivia_consumer.php
@@ -14,42 +14,84 @@ $channel->queue_declare('trivia_queue', false, false, false, false);
 
 function create_trivia($n)
 {
+    $action = null;
+    $trivia_id = null;
+    if (isset($n["action"])) {
+        $action = $n["action"];
+    } else {
+        $action = "create";
+    }
+    if (isset($n["trivia_id"])) {
+        $trivia_id = $n["trivia_id"];
+    }
     $user_id = $n["id"];
     $name = $n["trivia_name"];
     $description = $n["trivia_description"];
     $log_file_name = "trivia_consumer.log";
     write_log("Trivia Created by: " . $n['email'], $log_file_name);
-    $params = array(
-        ":name" => $name,
-        ":description" => $description,
-        ":visibility" => 0,
-        ":user_id" => $user_id
-    );
-    $db = getDB();
-    $query = "INSERT INTO Trivia(title, description, visibility, user_id) ";
-    $query .= "VALUES(:name, :description, :visibility, :user_id)";
-    $stmt = $db->prepare($query);
-    $r = $stmt->execute($params);
-    $e = $stmt->errorInfo();
-    if ($e[0] == "00000") {
-        $query = "SELECT * FROM Trivia WHERE user_id = :id";
+    if ($action == "create") {
+        $db = getDB();
+        $query = "INSERT INTO Trivia(title, description, visibility, user_id) ";
+        $query .= "VALUES(:name, :description, :visibility, :user_id)";
+        $params = array(
+            ":name" => $name,
+            ":description" => $description,
+            ":visibility" => 0,
+            ":user_id" => $user_id
+        );
         $stmt = $db->prepare($query);
-        $stmt->execute(array(":id" => $user_id));
-        $trivia_games = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $response = array(
-            "status" => "success",
-            "message" => "Trivia created successfully",
-            "trivia_games" => $trivia_games
+        $r = $stmt->execute($params);
+        $e = $stmt->errorInfo();
+        if ($e[0] == "00000") {
+            $query = "SELECT * FROM Trivia WHERE user_id = :id";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(":id" => $user_id));
+            $trivia_games = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $response = array(
+                "status" => "success",
+                "message" => "Trivia created successfully",
+                "trivia_games" => $trivia_games
+            );
+            write_log("Trivia created successfully", $log_file_name);
+        } else {
+            $response = array(
+                "status" => "error",
+                "message" => $e[2]
+            );
+            write_log("Trivia created error: " . $e[2], $log_file_name);
+        }
+        return $response;
+    } else if ($action == "edit") {
+        $db = getDB();
+        $query = "UPDATE Trivia SET title = :name, description = :description WHERE id = :id";
+        $params = array(
+            ":name" => $name,
+            ":description" => $description,
+            ":id" => $trivia_id
         );
-        write_log("Trivia created successfully", $log_file_name);
-    } else {
-        $response = array(
-            "status" => "error",
-            "message" => $e[2]
-        );
-        write_log("Trivia created error: " . $e[2], $log_file_name);
+        $stmt = $db->prepare($query);
+        $r = $stmt->execute($params);
+        $e = $stmt->errorInfo();
+        if ($e[0] == "00000") {
+            $query = "SELECT * FROM Trivia WHERE user_id = :id";
+            $stmt = $db->prepare($query);
+            $stmt->execute(array(":id" => $user_id));
+            $trivia_games = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $response = array(
+                "status" => "success",
+                "message" => "Trivia updated successfully",
+                "trivia_games" => $trivia_games
+            );
+            write_log("Trivia updated successfully", $log_file_name);
+        } else {
+            $response = array(
+                "status" => "error",
+                "message" => $e[2]
+            );
+            write_log("Trivia updated error: " . $e[2], $log_file_name);
+        }
+        return $response;
     }
-    return $response;
 }
 
 echo " [x] Awaiting RPC requests\n";
