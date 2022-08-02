f652e3b979ef89c258e82f0f6ffc506b413b67d9
dominic2486 <70656707+dominic2486@users.noreply.github.com>
2022-08-01 20:39:16 -0400

    fixing to ed25519 and clean up api

--- lib/scripts/migrateAPP.sh
+++ lib/scripts/migrateAPP.sh
@@ -3,17 +3,20 @@
 #34.138.248.175
 ./migrate.sh /home/$USER/Scripts/lib/scripts /home/$1/ $1 $2
 #the paths need to be generalized
-ssh -i ~/.ssh/id_rsa $1@$2 /home/$1/scripts/base.sh start $3 $1
+ssh $1@$2 /home/$1/scripts/base.sh start $3 $1
 ./migrate.sh /home/$USER/Group/IT490-M22-TBD1/static /home/$1/dropoff/static $1 $2
 ./migrate.sh /home/$USER/Group/IT490-M22-TBD1/partials /home/$1/dropoff/partials $1 $2
 ./migrate.sh /home/$USER/Group/IT490-M22-TBD1/vendor /home/$1/dropoff/vendor $1 $2
+./migrate.sh /home/$USER/Group/IT490-M22-TBD1/lib/ /home/$1/dropoff/ $1 $2
 ./migrate.sh /home/$USER/Group/IT490-M22-TBD1/lib/helpers.php /home/$1/dropoff/lib $1 $2
 ./migrate.sh /home/$USER/Group/IT490-M22-TBD1/lib/index.php /home/$1/dropoff/lib $1 $2
+./migrate.sh /home/$USER/Group/IT490-M22-TBD1/lib/index.php /home/$1/dropoff/lib $1 $2
 ./migrate.sh /home/$USER/Group/IT490-M22-TBD1/README.md /home/$1/dropoff $1 $2
+./migrate.sh /home/$USER/Group/IT490-M22-TBD1/Logo.png /home/$1/dropoff $1 $2
 #copies all php outside of the lib folder to the dropoff folder
 for file in /home/$USER/Group/IT490-M22-TBD1/*.php; do
 	./migrate.sh $file /home/$1/dropoff $1 $2
 done
-ssh -i ~/.ssh/id_rsa $1@$2 /home/$1/scripts/base.sh APP $3 $1
+ssh $1@$2 /home/$1/scripts/base.sh APP $3 $1
 #move changes to the live folder
-#ssh -i ~/.ssh/id_rsa $1@$2 /home/$1/scripts/do_Implement.sh dropoff
+ssh $1@$2 /home/$1/scripts/do_Implement.sh dropoff
