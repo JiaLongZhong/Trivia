c3bfbd0a8ea20a77cacbacacd2f4875e9961a0a4
dominic2486 <70656707+dominic2486@users.noreply.github.com>
2022-08-03 22:00:31 -0400

    leaderboard work #73

---
+++ leaderboard.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title><?php echo ucfirst(substr(basename(__FILE__), 0, -4)); ?></title>
+    <?php require_once(__DIR__ . "/partials/header.php"); ?>
+</head>
+
+<body>
+    <?php require_once(__DIR__ . "/partials/nav.php"); ?>
+    <?php
+    if (!is_logged_in()) {
+        error_msg("You do not have permission to access this page");
+        show_flash_messages();
+        die();
+    }
+    ?>
+    <?php
+
+    
+    ?>
+    <div class="container">
+        <div class = "row">
+            <div class="col-md-12">
+                <h1>Leaderboard</h1>
+                
+            </div>
+        </div> 
+    </div>
+
+    <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Score</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        require_once(__DIR__ . "/rpc_producer.php");
+                        $leaderboard_rpc = new RpcClient();
+                        $parm = array("user" => get_user_id());
+                        $response = json_decode($leaderboard_rpc->call($parm, 'leaderboard_queue'), true);
+                        if ($response["status"] == "success") {
+                            $leaderboard = $response["leaderboard"];
+                            foreach ($leaderboard as $user) {
+                                echo "<tr>";
+                                echo "<td>" . $user["fname"] . "</td>";
+                                echo "<td>" . $user["score"] . "</td>";
+                                echo "</tr>";
+                            }
+                        }
+                        ?>
+                    </tbody>
+
+    <?php include_once(__DIR__ . "/partials/footer.php"); ?>
+</body>
+
+</html>
---
+++ rpc_leaderboard_consumer.php
@@ -0,0 +1,83 @@
+<?php
+
+require_once(__DIR__ . '/lib/configrmq.php');
+require_once(__DIR__ . "/lib/helpers.php");
+require_once (__DIR__ . '/vendor/autoload.php');
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection($brokerhost, $brokerport, $brokeruser, $brokerpass);
+$channel = $connection->channel();
+
+$channel->queue_declare('leaderboard_queue', false, false, false, false);
+
+
+function LeaderboardSubmit($n)
+{
+	$log_file_name = "leaderboard_consumer.log";
+	write_log("leaderboard Request from: " . $n['user'], $log_file_name);
+	$params = array(
+	);
+	$db = getDB();
+	$query = "SELECT  u.fname, s.score, s.user_id, 
+	FROM score s 
+	JOIN Users u
+	WHERE s.user_id = ANY 
+	(SELECT sender_id FROM friend_request WHERE receiver_id = :user_id
+	UNION
+	SELECT receiver_id FROM friend_request WHERE sender_id = :user_id)
+	SORT BY s.score DESC 
+	LIMIT 20";
+	$stmt = $db->prepare($query);
+	$r = $stmt->execute();
+	$e = $stmt->errorInfo();
+	if ($e[0] == "00000") {
+		$response = array(
+			"status" => "success",
+			"message" => "Record added successfully"
+		);
+		write_log("leaderboard sent successfully: " . $n["user"], $log_file_name);
+		
+	} elseif ($e[0] == "23000") {
+		$response = array(
+			"status" => "error",
+			"message" => "user already exists for that score"
+            #TODO add a check to see if the score is the same as the previous one and if so, don't add it again
+		);
+		write_log("leaderboard sent error: " . $e[2], $log_file_name);
+	} else {
+		$response = array(
+			"status" => "error1",
+			"message" => $e[2]
+		);
+		write_log("leaderboard sent error: " . $e[2], $log_file_name);
+	}
+	return $response;
+}
+
+echo " [x] Awaiting RPC requests for score\n";
+$callback = function ($req) {
+	$n = $req->body;
+	$consumed_data = json_decode($n, true);
+	$return_msg = json_encode(LeaderboardSubmit($consumed_data));
+	$msg = new AMQPMessage(
+		$return_msg,
+		array('correlation_id' => $req->get('correlation_id'))
+	);
+
+	$req->delivery_info['channel']->basic_publish(
+		$msg,
+		'',
+		$req->get('reply_to')
+	);
+	$req->ack();
+};
+
+$channel->basic_qos(null, 1, null);
+$channel->basic_consume('score_queue', '', false, false, false, false, $callback);
+while (count($channel->callbacks) || $channel->is_consuming() || $channel->is_open()) {
+	$channel->wait();
+}
+$channel->close();
+$connection->close();
