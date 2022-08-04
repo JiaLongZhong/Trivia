3c0faaf0e09d05d5b077f501976b87e6a5132a89
dominic2486 <70656707+dominic2486@users.noreply.github.com>
2022-08-04 16:13:16 -0400

    fix capitalization

--- sql/009_create_table_scores.sql
+++ sql/009_create_table_scores.sql
@@ -1,9 +1,11 @@
-CREATE TABLE scores
+CREATE TABLE Score
 (
     id        int auto_increment,
     user_id   int,
     trivia_id int,
     score     int,
+    `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    ,`modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     primary key (id),
     FOREIGN KEY (user_id) REFERENCES Users (id),
     FOREIGN KEY (trivia_id) REFERENCES Trivia (id)
